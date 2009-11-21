cafe1f50fc242160bdb1c855448fb3952063702a
Gilbert L <gilbert.kf.leung@gmail.com>
2009-11-20 23:22:24 -0800

    switched to leiningen

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
-(defproject leiningen "0.5.0"
+(defproject mongo-clojure-wrapper "0.1.0"
+  :description "A MongoDB wrapper around official MongoDB's java driver 
+                (mongo-java-driver)"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [com.mongodb/mongo-java-driver "1.0"]
-                 [jline/jline "0.9.94"]])
-                 ;[ant/ant-launcher "1.6.2"]
-                 ;[org.apache.maven/maven-ant-tasks "2.0.10"]]
-  ;:dev-dependencies [[org.clojure/swank-clojure "1.0"]])
-
+                 [com.mongodb/mongo-java-driver "1.0"]])
